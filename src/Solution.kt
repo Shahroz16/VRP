9cf368bf7e2e7fcde7372ea34389d19be5aee244
Shahroz Khan <shrozali@gmail.com>
2017-12-13 03:23:22 +0500

    Documentation added

--- src/Solution.kt
+++ src/Solution.kt
@@ -1,6 +1,4 @@
 import ListPartitioner.getAllPartitions
-import java.util.*
-import java.util.HashMap
 import java.util.ArrayList
 import java.util.concurrent.TimeUnit
 
@@ -19,14 +17,20 @@ fun main(args: Array<String>) {
     val startTimeMillis = System.currentTimeMillis()
     val shortestRouteSet = shortestRouteWithPartitions(locationIds, vehicles)
     println("Solution time: ${TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - startTimeMillis)} seconds")
-    System.out.printf("Shortest route time: %.1f minutes\n", maxLengthForRouteSet(shortestRouteSet));
+    System.out.printf("Shortest route time: %.1f minutes\n", maxLengthForRoutes(shortestRouteSet));
     println("Shortest route: " + shortestRouteSet)
 }
 
+/**
+ *Distance between first and last and consecutive elements of a list.
+ **/
 fun distance(x: Int, y: Int): Double {
     return distances[x, y]
 }
 
+/**
+ * Distance between first and last and consecutive elements of a list
+ */
 fun routeLength(route: List<Int>): Double {
     var sum = 0.0
     for (i in 1 until route.size) {
@@ -36,12 +40,18 @@ fun routeLength(route: List<Int>): Double {
     return sum
 }
 
+/**
+ * Returns minimum from a list based on route length
+ */
 fun shortestRoute(routes: ArrayList<ArrayList<Int>>): ArrayList<Int> {
     return routes.minBy {
         routeLength(it)
     }!!
 }
 
+/**
+ * Return all permutations of a list, each starting with the first item
+ */
 fun allRoutes(original: ArrayList<Int>): ArrayList<ArrayList<Int>> {
     if (original.size < 2) {
         return arrayListOf(original)
@@ -54,21 +64,33 @@ fun allRoutes(original: ArrayList<Int>): ArrayList<ArrayList<Int>> {
     }
 }
 
-fun maxLengthForRouteSet(routeSet: List<List<Int>>): Double {
+/**
+ * Return maximum from a given route list
+ */
+fun maxLengthForRoutes(routeList: List<List<Int>>): Double {
     val routeLengths = ArrayList<Double>()
-    return routeSet.mapTo(routeLengths) {
+    return routeList.mapTo(routeLengths) {
         routeLength(it)
     }.max()!!
 }
 
+/**
+ * This function receives all k-subsets of a route and returns the subset
+ * with minimum distance cost. Note the total time is always equal to
+ * the max time taken by any single vehicle
+ */
 fun shortestRouteWithPartitions(locationIds: List<Int>, partitions: Int): List<List<Int>> {
     return allShortRoutesWithPartitions(locationIds, partitions)
             .distinct()
             .minBy {
-                maxLengthForRouteSet(it)
+                maxLengthForRoutes(it)
             }!!
 }
 
+/**
+ * Our partitions represent number of vehicles. This function yields
+ * an optimal path for each vehicle given the destinations assigned to it
+ */
 fun allShortRoutesWithPartitions(seq: List<Int>, vehicles: Int): ArrayList<List<List<Int>>> {
     val shortRoutesList = ArrayList<List<List<Int>>>()
     return getAllPartitions(seq.drop(1)).filter {
